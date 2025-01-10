e843ec57b507dc65634b2eeabe1311222ab78fc1
horw00d <cameron.j.horwood@gmail.com>
2025-01-10 07:12:00 -0400

    First working version, preparing for AWS cloud hosting

---
+++ client/src/app/users/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useGetUsersQuery } from '@/state/api'
+import Header from '../(components)/Header';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { } from '@mui/x-data-grid/hooks/features/rows/gridRowsUtils';
+
+const columns: GridColDef[] = [
+    {field: "userId", headerName: "ID", width: 90},
+    {field: "name", headerName: "Name", width: 200 },
+    {field: "email", headerName: "Email", width: 200 }
+]
+
+const Users = () => {
+    const { data: users, isError, isLoading } = useGetUsersQuery();
+    
+    if(isLoading) { 
+        return <div className="py-4">Loading...</div>;
+    }
+
+    if(isError || !users){
+        return(
+            <div className="text-center text-red-500 py-4">
+                Failed to fetch users
+            </div>
+        )
+    }
+
+  return (
+    <div className="flex flex-col">
+        <Header name="Users" />
+        <DataGrid
+            className="bg-white shadown rounded-lg border border-gray-200 mt-5 !text-gray-700"
+            rows={users}
+            columns={columns}
+            getRowId={(row) => row.userId}
+            checkboxSelection
+        />
+    </div>
+  )
+}
+
+export default Users
